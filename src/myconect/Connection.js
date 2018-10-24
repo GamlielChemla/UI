cc7337deb388e56596b25ec0b418919afd0180bb
gamliel <gamliel.chemla@gmail.com>
2018-10-24 16:01:36 +0300

    24.10.18 16:00

---
+++ src/myconect/Connection.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Connection extends Component {
+ 
+ 
+    render() {
+      
+      return (
+        <div >
+
+{this.props.onChange}
+
+            </div>
+
+      )
+      }
+
+      }
+export default Connection;
